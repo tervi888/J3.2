2e0d20723d3803c861f79af9238a69015e4bbc15
Tervi test <Tervi@list.ru>
2023-04-15 13:35:00 +0300

    bmi

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate (int weight, double height) {
+        double heightSquared = (height * height);
+        double bmi = weight / heightSquared;
+        return (int) bmi;
+
+    }
+}
